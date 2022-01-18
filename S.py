10166f9fd1ac4009444a0708855f58db59314be4
REBEL725 <81420701+REBEL725@users.noreply.github.com>
2022-01-19 01:20:43 +0530

    Update and rename String.py to S.py

--- S.py
+++ S.py
@@ -15,11 +15,11 @@ while True:
             session = client.session.save()
             client.send_message(
                 "me",
-                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` \n\n And Visit @MafiaBot_Support For Any Help!\n\n",
+                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` \n\n And Visit @REBELBOT_SUPPORT For Any Help!\n\n",
             )
 
             print(
-                "Thanks for Choosing MafiaBot Have A Good Time....Note That When You Terminate the Old Session ComeBack And Genrate A New String Session Old One Wont Work"
+                "Thanks for Choosing rebelbot Have A Good Time....Note That When You Terminate the Old Session ComeBack And Genrate A New String Session Old One Wont Work"
             )
     except:
         print("")
